58f12fb53798aa43e2c3bbab77435b6598c6fbf2
dev-javascript <javascript.code.dev@gmail.com>
2024-05-22 22:01:23 +0330

    adding types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+
+export type ITabsMoreButton = {
+    resize: (selectedTabIndex: number, direction?: "ltr" | "rtl", isVertical?: boolean) => Array<{ el: HTMLElement, index: Number }>;
+};
+declare class tabsMoreButton implements ITabsMoreButton {
+    constructor(options: {
+        containerDisplay?: string;
+        tabDisplay?: string;
+        buttonElement: HTMLElement;
+        containerElement: HTMLElement;
+    });
+    public resize(selectedTabIndex: number, direction?: "ltr" | "rtl", isVertical?: boolean): Array<{ el: HTMLElement, index: Number }>;
+}
+export default tabsMoreButton;
