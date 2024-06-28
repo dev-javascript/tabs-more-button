e244e56e20209f79405d15480eeadbc8fb59480c
dev-javascript <javascript.code.dev@gmail.com>
2024-06-28 21:17:35 +0330

    update types

--- index.d.ts
+++ index.d.ts
@@ -8,6 +8,7 @@ declare class tabsMoreButton implements ITabsMoreButton {
         tabDisplay?: string;
         buttonElement: HTMLElement;
         containerElement: HTMLElement;
+        tablistElement: HTMLElement;
     });
     public resize(selectedTabIndex: number, direction?: "ltr" | "rtl", isVertical?: boolean): Array<{ el: HTMLElement, index: Number }>;
 }
